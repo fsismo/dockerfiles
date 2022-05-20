9b9125ac76c2dc13572ff4a494223cc7dae2b9fe
Fernando Sismonda <fernando@sismonda.com.ar>
2022-05-20 18:06:58 -0300

    Bugfixed for ubuntu-2204-php-wordpres new docker compose file

--- ubuntu-22.04-php-wordpress/opt/utils/services.sh
+++ ubuntu-22.04-php-wordpress/opt/utils/services.sh
@@ -2,6 +2,8 @@
 mkdir -p /var/www/html
 rm -r /var/www/html/index.html
 if [ -z "$(ls -A /var/www/html)" ]; then
-    sudo docker container stop ubuntu-22.04-php-wordpress && sudo docker container rm ubuntu-22.04-php-wordpress
+    curl https://wordpress.org/latest.tar.gz | tar zx -C /tmp/ 
+    mv /tmp/wordpress/* /var/www/html
+    chown -R www-data.www-data /var/www/html
 fi
 apachectl -D FOREGROUND
