8e768d32cd01fb4659c6e5869c10e4b2bc84a73d
Fernando Sismonda <fernando@sismonda.com.ar>
2022-05-23 12:26:33 -0300

    Fixed chmod in services.sh

--- mariadb-10.6-focal-auto-backups/opt/utils/services.sh
+++ mariadb-10.6-focal-auto-backups/opt/utils/services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "BACKUP_RETENTION="$(echo $BACKUP_RETENTION) > /opt/dbbackup/.config.sh
 echo "MARIADB_ROOT_PASSWORD="$(echo $MARIADB_ROOT_PASSWORD) >> /opt/dbbackup/.config.sh
-chown 400 /opt/dbbackup/.config.sh
+chmod 500 /opt/dbbackup/.config.sh
 /etc/init.d/cron start
