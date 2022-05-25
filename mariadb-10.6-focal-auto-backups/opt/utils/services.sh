afa20ad6379443d518dc6b835cbec4acfd3f8f94
Fernando Sismonda <fernando@sismonda.com.ar>
2022-05-25 17:56:09 -0300

    Bugfixes

--- mariadb-10.6-focal-auto-backups/opt/utils/services.sh
+++ mariadb-10.6-focal-auto-backups/opt/utils/services.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "BACKUP_RETENTION="$(echo $BACKUP_RETENTION) > /opt/dbbackup/.config.sh
+echo "#!/bin/bash" > /opt/dbbackup/.config.sh
+echo "BACKUP_RETENTION="$(echo $BACKUP_RETENTION) >> /opt/dbbackup/.config.sh
 echo "MARIADB_ROOT_PASSWORD="$(echo $MARIADB_ROOT_PASSWORD) >> /opt/dbbackup/.config.sh
 chmod 500 /opt/dbbackup/.config.sh
 /etc/init.d/cron start
