c1986a2fda6eb2ab148262ca83454d99c3b5824a
Fernando Sismonda <fernando@sismonda.com.ar>
2022-06-16 12:00:42 -0300

    Housekeeping.

--- mariadb-10.6-focal-auto-backups/opt/utils/services.sh
+++ mariadb-10.6-focal-auto-backups/opt/utils/services.sh
@@ -2,5 +2,4 @@
 echo "#!/bin/bash" > /opt/dbbackup/.config.sh
 echo "BACKUP_RETENTION="$(echo $BACKUP_RETENTION) >> /opt/dbbackup/.config.sh
 echo "MARIADB_ROOT_PASSWORD="$(echo $MARIADB_ROOT_PASSWORD) >> /opt/dbbackup/.config.sh
-chmod 500 /opt/dbbackup/.config.sh
-/usr/sbin/cron
+/usr/sbin/cron &
